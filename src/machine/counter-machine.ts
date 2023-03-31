5edd61771fe482927f6f5c0f8cbd13f51bb45a85
rldnrl <devpark1002@gmail.com>
2023-03-31 22:23:27 +0900

    feat: add disable state

--- src/machine/counter-machine.ts
+++ src/machine/counter-machine.ts
@@ -2,27 +2,38 @@ import { assign, createMachine } from "xstate";
 
 export const counterMachine = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2UoBswDoDyAdgMRobYDaADALqKgAOqsAlgC7OoF0gAeiAjADYA7DgBMAZgAcYqfzEBWADQgAngIUSclCQE5KuiZX7CFwuWIC+llaSy48AM0cl09qrSQhGLdp258CAC0-ApS4hbKahpaOvqGxqbm8tY2IASoEHDcdtjcPmwcXF6BQWKUghHyUerBlDi6jbqCugAsMq1i-FKm1rZu2PjFDEyF-iWI7Sq1UpUKlAtGCiZmFn0guQ7O+aN+w7yTUtOIYl3aCzqUy0lrqUA */
+    /** @xstate-layout N4IgpgJg5mDOIC5QBUD2UoBswGIDaADALqKgAOqsAlgC5WoB2pIAHogLQDMAjJwHQAOAJwCALJwDsAVgA0IAJ6JufTgCZRq7gOkBfHXLQZsfAKIMAhgCNsEPgHkGOQ1jCESSEBWp1GzNgk0BPlUCMQJRAgIhDSlOWQVEVSkJQQEBTgJuCUluKW4ANny9A3QXUwtrSHsAM2qnUuw3Zi9aeiYPfxC+KJ5RKQIJIXDRCPjFAOTU9MzsiVyCouKQBlQIOGZnbGbKVt8Ojk585WExSTGOAXy+LOTBnilkwoElzbA+ABEqWCsbbe82vwcXJBVQCbhJOTjOJSPj9SIZPLSCRgl4NN5mH6QP67dqgfzsVQpIb5ARSURCEJCB5SKmQxDSWHCIbcAak7hU1Soozoio2ey4zw7HwC-wFGGqaLcCJSgqiEmiOkISRXDRU0QCTT9TgjLllDGVWx2WrY4WAhC5IQqKWEwYaSSHRUFfihETpQ4skSiPR6IA */
     id: "Toggle",
-    initial: "On",
+    initial: "Enabled",
     context: {
       count: 0,
     },
     states: {
-      On: {
-        on: {
-          Toggle: {
-            actions: "incrementToggle",
-            target: "Off",
+      Disabled: {},
+      Enabled: {
+        initial: "On",
+        states: {
+          On: {
+            on: {
+              Toggle: {
+                actions: "incrementToggle",
+                target: "Off",
+              },
+            },
+          },
+
+          Off: {
+            on: {
+              Toggle: {
+                actions: "incrementToggle",
+                target: "On",
+              },
+            },
           },
         },
-      },
-      Off: {
-        on: {
-          Toggle: {
-            actions: "incrementToggle",
-            target: "On",
-          },
+        always: {
+          target: "Disabled",
+          cond: "Count greater than 5",
         },
       },
     },
@@ -34,6 +45,11 @@ export const counterMachine = createMachine(
           return context.count + 1;
         },
       }),
+    },
+    guards: {
+      "Count greater than 5": (context) => {
+        return context.count > 5;
+      },
     },
   }
 );
