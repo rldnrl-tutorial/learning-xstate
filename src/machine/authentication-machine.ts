e897bc550e5fcf6c340c5c3182be47ef29361cd2
rldnrl <devpark1002@gmail.com>
2023-04-01 17:21:00 +0900

    feat: add invoke props

--- src/machine/authentication-machine.ts
+++ src/machine/authentication-machine.ts
@@ -1,32 +1,94 @@
-import { createMachine } from "xstate";
+import { actions, createMachine } from "xstate";
 
-export const authenticationMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QEECuAXAFmAduglgMYCGBA9jgHQDC2hA1vjlAASqxgBOLADlwLb5YsfBVgsANmABuYCQGIACgKEixLCGHTF8ElpzCEw+WRADaABgC6iUDzIjyOWyAAeiALQBGAMyULAGwATADsACw+ISEAnBbR8WEhADQgAJ6IXgAcmZReAWERmUFxPtFB0T4AvpUpaFi4BCRONHSMzGwc3HycgsKiOOJSsgrKPar94prauvqGxqZmXjZIIPaO-S7uCB4+QbkBRUF5Fj6BIZkArCnpCKEBlPnZwRfnPl7RYdW1GNh4RKT9FqGNqsdhcXgqPrqIZyJSQtQDDRaHR6AxGEyQMxBZZ2Bz4JybTx+MKZCwhXaZD7BPLRC4Ba6IC5vSiRCykooWC5BCJeao1EA4MiaeArOq-RoAiguNb4jYrLYeUIhSiJaJ5LwXMIBJlFBnbHx+TI+C500LRTIBLxhblfEBihr-Zq0YFMUGdCFjKGImESaV4gnyzzFPylEJeIKXE4G7V6pkPfJhLInALagLRW32v5NQEAOTILFGvQRfvWUsD2yC5RVMXVmu1PkOseZrPZxS5PIzPwd2YolDzPWIvpWMoDoAVwZZ0TDEYuUZ82qCerefi8FneFuCIWCFk+-MzEuayAggmcw-9crHQYsFkoF2iAR8BTO5NOyTSGWy+yf5zZRwuFr5SogA */
-  tsTypes: {} as import("./authentication-machine.typegen").Typegen0,
-  id: "Authentication",
-  initial: "Checking user permissions level",
-  states: {
-    "Checking user permissions level": {
-      on: {
-        "Permissions detail received": [
-          {
-            target: "Admin",
-            cond: "User is admin",
-          },
-          {
-            target: "Normal",
-            actions: [
-              "Say hello to normal user",
-              "Report to API that normal user logged in",
-            ],
-            cond: "User is Normal",
-          },
-          "No Permission",
-        ],
+export const authenticationMachine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5QEECuAXAFmAduglgMYCGBA9jgHQDC2hA1vjlAASqxgBOLADlwLb5YsfBVgsANmABuYCQGIIFMJSbSy9FWiy4CJclVpgGTVuy68BQkWMky5CNWX2icAbQAMAXU9fEoHjIRA38QAA9EAEZKACYPAE4AdgAOABZEgGZ4mKyY+IA2VIAaEABPKPiPSmSU-JiAVkj8+pjk+oyAXw6S7Ww8IlJXGjpGZjYObj5OQWFXcSlZBSUcFSdNSl7dAYNh41GzCctp6zm7RcccdRcKXzdIvyQQQODXUIiEAFpIxOr4+vy0vl8kkEhkGiVyggYol4pQMvkMh5gU0UpEPIl6l0ehg+npBhRdiYxuZJlZZrYFnJFMpVJcNFocVtroYRqZxhYpjMbDh5vYJBcrvj3N43DEHgEgvgQo93mjKDDIslvoqARkMqlIhkIYh2qlKB42h4Gv8cpEGliQJt+szCft2aTjuSeWcqVxOGROJQeBJSAAzD38DaM61C21sklHLmnSn8pzM27eULPKWvGWIL6wjwGgqpVoY+IZeptbUIIHJShF1LxP4eeqpGr5C1WvE7IxEg4csnc3mLeRuj1en3of3TIM6EOt1nEw6ck4UvkC5xC273JOS6Wgd6JH4ZSJo2uJZVZyIlmHVWqpeoJZKIhVN4MtoZtu0R2dOntU6jEHCEOS+NcvBQbyIDEhSxDEqSXi0kFoqBiQlh8dYVkW9SJM0NQtLq97jo+BLPuGM5dtGfLyGEsDoKQKjEL66BcAAFJEV4eAAlPIzbbE+U4dg6Ubzos-6PMmG7hOmV7RLWRpJHukQFChJYtJQlSJB4YJJIWkF1thuIcQSAAq+D8GALBkBg8gAEpgOgnClAJEqATgwGfIWPx1jkapxKk6LJKBJaFrCyT5DCGTKQ0kkFF03QgDgZAQHAoTscyAEpkBaafHWmZXnkh57rJyQIZEub6ihSr1nEoHfFpTKhvh06do63YuhISXCe8HxlZQiqZKkwWFAkiQxDE8l6jUAXqgNirQpelUTkMAByZAsAACkRKV2clDmpW1rSxMCPXJOieTJH88lghWhoQVkHgasF024VQ83TMQTWCeuqabum7WdeqPWeUk4JlIgu56rudRGkq0KVH8t06VQyAQIIG1rS16YybCGQBfU9QFq0VboyWipo7UOTVmqAWpNDNr6YZxkYM1b0iZ8A3lqBBZoft-XxEd9QlkCPwGmJOVqmayQU9V36-hIUgQHTq0M3W0R1JUQLBZ50KDQDCApHCtSFtCZoagVEUdEAA */
+    tsTypes: {} as import("./authentication-machine.typegen").Typegen0,
+    id: "Authentication",
+    initial: "Checking user permissions level",
+    schema: {} as {
+      services: {
+        "Check user permissions": {
+          data: {
+            user: {
+              permissions: string[];
+            };
+          };
+        };
+      };
+    },
+    states: {
+      "Checking user permissions level": {
+        invoke: {
+          src: "Check user permissions",
+          onDone: [
+            {
+              target: "Admin",
+              cond: "User is admin",
+            },
+            {
+              target: "Normal",
+              actions: [
+                "Say hello to normal user",
+                "Report to API that normal user logged in",
+              ],
+              cond: "User is Normal",
+            },
+            {
+              target: "No Permission",
+            },
+          ],
+          onError: [
+            {
+              cond: "Error is timeout error",
+              target: "Time out",
+            },
+            {
+              target: "No Permission",
+            },
+          ],
+        },
+
+        on: {
+          Cancel: "Cancelled",
+        },
+
+        after: {
+          "1500": "Time out",
+        },
+      },
+
+      "No Permission": {},
+      Normal: {},
+      Admin: {},
+
+      "Time out": {
+        on: {
+          Retry: "Checking user permissions level",
+        },
+      },
+
+      Cancelled: {},
+    },
+  },
+  {
+    guards: {
+      "User is admin": (_, event) => {
+        return event.data.user.permissions.includes("admin");
+      },
+      "User is Normal": (_, event) => {
+        return event.data.user.permissions.includes("normal");
       },
     },
-    "No Permission": {},
-    Normal: {},
-    Admin: {},
-  },
-});
+    services: {
+      "Check user permissions": async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1200));
+        return {
+          user: {
+            permissions: ["normal"],
+          },
+        };
+      },
+    },
+  }
+);
--- src/machine/authentication-machine.typegen.ts
+++ src/machine/authentication-machine.typegen.ts
@@ -1,33 +1,39 @@
-// This file was automatically generated. Edits will be overwritten
 
-export interface Typegen0 {
-  "@@xstate/typegen": true;
-  internalEvents: {
-    "xstate.init": { type: "xstate.init" };
-  };
-  invokeSrcNameMap: {};
-  missingImplementations: {
-    actions:
-      | "Report to API that normal user logged in"
-      | "Say hello to normal user";
-    delays: never;
-    guards: "User is Normal" | "User is admin";
-    services: never;
-  };
-  eventsCausingActions: {
-    "Report to API that normal user logged in": "Permissions detail received";
-    "Say hello to normal user": "Permissions detail received";
-  };
-  eventsCausingDelays: {};
-  eventsCausingGuards: {
-    "User is Normal": "Permissions detail received";
-    "User is admin": "Permissions detail received";
-  };
-  eventsCausingServices: {};
-  matchesStates:
-    | "Admin"
-    | "Checking user permissions level"
-    | "No Permission"
-    | "Normal";
-  tags: never;
-}
+  // This file was automatically generated. Edits will be overwritten
+
+  export interface Typegen0 {
+        '@@xstate/typegen': true;
+        internalEvents: {
+          "done.invoke.Authentication.Checking user permissions level:invocation[0]": { type: "done.invoke.Authentication.Checking user permissions level:invocation[0]"; data: unknown; __tip: "See the XState TS docs to learn how to strongly type this." };
+"error.platform.Authentication.Checking user permissions level:invocation[0]": { type: "error.platform.Authentication.Checking user permissions level:invocation[0]"; data: unknown };
+"xstate.after(1500)#Authentication.Checking user permissions level": { type: "xstate.after(1500)#Authentication.Checking user permissions level" };
+"xstate.init": { type: "xstate.init" };
+        };
+        invokeSrcNameMap: {
+          "Check user permissions": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+        };
+        missingImplementations: {
+          actions: "Report to API that normal user logged in" | "Say hello to normal user";
+          delays: never;
+          guards: "Error is timeout error";
+          services: never;
+        };
+        eventsCausingActions: {
+          "Report to API that normal user logged in": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+"Say hello to normal user": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+        };
+        eventsCausingDelays: {
+          
+        };
+        eventsCausingGuards: {
+          "Error is timeout error": "error.platform.Authentication.Checking user permissions level:invocation[0]";
+"User is Normal": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+"User is admin": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+        };
+        eventsCausingServices: {
+          "Check user permissions": "Retry" | "xstate.init";
+        };
+        matchesStates: "Admin" | "Cancelled" | "Checking user permissions level" | "No Permission" | "Normal" | "Time out";
+        tags: never;
+      }
+  
