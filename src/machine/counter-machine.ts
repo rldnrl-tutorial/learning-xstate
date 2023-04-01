050c9c2144a4500bfb3e207fed635fa6ca148872
rldnrl <devpark1002@gmail.com>
2023-04-01 23:53:54 +0900

    feat: define type

--- src/machine/counter-machine.ts
+++ src/machine/counter-machine.ts
@@ -1,6 +1,14 @@
 import { assign, createMachine } from "xstate";
 
-export const counterMachine = createMachine(
+type CounterContext = {
+  count: number;
+};
+
+type CounterEvents = {
+  type: "Toggle";
+};
+
+export const counterMachine = createMachine<CounterContext, CounterEvents>(
   {
     /** @xstate-layout N4IgpgJg5mDOIC5QBUD2UoBswGIDaADALqKgAOqsAlgC5WoB2pIAHogLQDMAjJwHQAOAJwCALJwDsAVgA0IAJ6JufTgCZRq7gOkBfHXLQZsfAKIMAhgCNsEPgHkGOQ1jCESSEBWp1GzNgk0BPlUCMQJRAgIhDSlOWQVEVSkJQQEBTgJuCUluKW4ANny9A3QXUwtrSHsAM2qnUuw3Zi9aeiYPfxC+KJ5RKQIJIXDRCPjFAOTU9MzsiVyCouKQBlQIOGZnbGbKVt8Ojk585WExSTGOAXy+LOTBnilkwoElzbA+ABEqWCsbbe82vwcXJBVQCbhJOTjOJSPj9SIZPLSCRgl4NN5mH6QP67dqgfzsVQpIb5ARSURCEJCB5SKmQxDSWHCIbcAak7hU1Soozoio2ey4zw7HwC-wFGGqaLcCJSgqiEmiOkISRXDRU0QCTT9TgjLllDGVWx2WrY4WAhC5IQqKWEwYaSSHRUFfihETpQ4skSiPR6IA */
     id: "Toggle",
