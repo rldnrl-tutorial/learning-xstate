200633999f9722a4a29d0b98c14697c82d205638
rldnrl <devpark1002@gmail.com>
2023-04-01 23:52:49 +0900

    feat: update type

--- src/machine/authentication-machine.typegen.ts
+++ src/machine/authentication-machine.typegen.ts
@@ -1,39 +1,52 @@
+// This file was automatically generated. Edits will be overwritten
 
-  // This file was automatically generated. Edits will be overwritten
-
-  export interface Typegen0 {
-        '@@xstate/typegen': true;
-        internalEvents: {
-          "done.invoke.Authentication.Checking user permissions level:invocation[0]": { type: "done.invoke.Authentication.Checking user permissions level:invocation[0]"; data: unknown; __tip: "See the XState TS docs to learn how to strongly type this." };
-"error.platform.Authentication.Checking user permissions level:invocation[0]": { type: "error.platform.Authentication.Checking user permissions level:invocation[0]"; data: unknown };
-"xstate.after(1500)#Authentication.Checking user permissions level": { type: "xstate.after(1500)#Authentication.Checking user permissions level" };
-"xstate.init": { type: "xstate.init" };
-        };
-        invokeSrcNameMap: {
-          "Check user permissions": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
-        };
-        missingImplementations: {
-          actions: "Report to API that normal user logged in" | "Say hello to normal user";
-          delays: never;
-          guards: "Error is timeout error";
-          services: never;
-        };
-        eventsCausingActions: {
-          "Report to API that normal user logged in": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
-"Say hello to normal user": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
-        };
-        eventsCausingDelays: {
-          
-        };
-        eventsCausingGuards: {
-          "Error is timeout error": "error.platform.Authentication.Checking user permissions level:invocation[0]";
-"User is Normal": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
-"User is admin": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
-        };
-        eventsCausingServices: {
-          "Check user permissions": "Retry" | "xstate.init";
-        };
-        matchesStates: "Admin" | "Cancelled" | "Checking user permissions level" | "No Permission" | "Normal" | "Time out";
-        tags: never;
-      }
-  
+export interface Typegen0 {
+  "@@xstate/typegen": true;
+  internalEvents: {
+    "done.invoke.Authentication.Checking user permissions level:invocation[0]": {
+      type: "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+      data: unknown;
+      __tip: "See the XState TS docs to learn how to strongly type this.";
+    };
+    "error.platform.Authentication.Checking user permissions level:invocation[0]": {
+      type: "error.platform.Authentication.Checking user permissions level:invocation[0]";
+      data: unknown;
+    };
+    "xstate.after(1500)#Authentication.Checking user permissions level": {
+      type: "xstate.after(1500)#Authentication.Checking user permissions level";
+    };
+    "xstate.init": { type: "xstate.init" };
+  };
+  invokeSrcNameMap: {
+    "Check user permissions": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+  };
+  missingImplementations: {
+    actions:
+      | "Report to API that normal user logged in"
+      | "Say hello to normal user";
+    delays: never;
+    guards: "Error is timeout error";
+    services: never;
+  };
+  eventsCausingActions: {
+    "Report to API that normal user logged in": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+    "Say hello to normal user": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+  };
+  eventsCausingDelays: {};
+  eventsCausingGuards: {
+    "Error is timeout error": "error.platform.Authentication.Checking user permissions level:invocation[0]";
+    "User is Normal": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+    "User is admin": "done.invoke.Authentication.Checking user permissions level:invocation[0]";
+  };
+  eventsCausingServices: {
+    "Check user permissions": "Retry" | "xstate.init";
+  };
+  matchesStates:
+    | "Admin"
+    | "Cancelled"
+    | "Checking user permissions level"
+    | "No Permission"
+    | "Normal"
+    | "Time out";
+  tags: never;
+}
