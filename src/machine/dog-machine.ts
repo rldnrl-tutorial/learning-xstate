76b0b76dae552fdd1e92325d53bec7650d1ebbba
rldnrl <devpark1002@gmail.com>
2023-04-01 17:20:42 +0900

    feat: add after props

--- src/machine/dog-machine.ts
+++ src/machine/dog-machine.ts
@@ -1,7 +1,7 @@
 import { createMachine } from "xstate";
 
 export const dogMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QBED2UB0BBWAbMYADgMQDKAhgLa66wAEAZqqhANoAMAuoqIarAEsALgNQA7HiAAeiAIwAWAEwZ582QA55AVgDsmgMyLF89QBoQAT0QBaWVvkYdW-ey2KAbPICc7RTsUAvgHmaJg4+ETEADKoAK4QdGKoArBgHNxIIHyCIuKSMgi6juo6+vpeiupGFRrmVgjWXg7qsu766l7uXm6KhvJBIejYAO7kANZgxABi5DT05HgEhOmS2cKiEpkF1ors7I7uWu6KWnU2uxj6rfLsx1oDIKEj42AYpBGEFtFxCUkpaVxVvx1nktjY7MpjIoNE4zggNOoMO51PotC0POp3K1Og8nlhRhMMAAhMBQKACMRQYgAcQEADcwHQACoAJzA5CEK0ya1ym1A238GHY6l8vVulUqXi8cK0rgwsi8slk7GVR2qOn6DySEDgklCQJyG3y5xMBzVp0sJshznY8jK+m0uy8uKG4SWBpBfOk4K8+gwVXYVxOcMazWu4vuwUeroJYA9vONDV2+yhMIt9XaSJRWh8aKDhnULrCsbeH3qvGBCbBDQUWn9uyD6bkHnl2fRyKxXXcReehNIAGNyGyIPGjdXbNp64HZMHLQgPPtkajkXp-GVnVG8SWSWSKVBR6D+eC3BhUyUmwhynWNKi-P57O5fEEgkA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBED2UB0BBWAbMYADgMQDKAhgLa66wAEAZqqhANoAMAuoqIarAEsALgNQA7HiAAeiAIwAWAEwZ582QA55AVgDsmgMyLF89QBoQAT0QBaWVvkYdW-ey2KAbPICc7RTsUAvgHmaJg4+ETEADKoAK4QdGKoArBgHNxIIHyCIuKSMgi6juo6+vpeiupGFRrmVgjWXg7qsu766l7uXm6KhvJBIejYAO7kANZgxABi5DT05HgEhOmS2cKiEpkF1ors7I7uWu6KWnU2uxj6rfLsx1oDIKEj42AYpBGEFtFxCUkpaVxVvx1nktjY7MpjIoNE4zggNOoMO51PotC0POp3K1Og8nlhRhMMAAhMBQKACMRQYgAcQEADcwHQACoAJzA5CEK0ya1ym1A238GHY6l8vVulUqXi8cK0rgwsi8slk7GVR2qOn6wUeQ3CS2IUlgQg5r3IDCEYBZAApZewAJTEPGLIhc3jA3n5GyGfY6Q53OHWQyXa6+WT6dw+rRKoJapIQOCSUJAnIbD0NMOIiN+yznJQYE4uXT2VGVXE6p2EJMgvnScFefQYKrsK4nf1NBvBu6lsIEsCV91ghq7fZQmGnbMIdpIlG6NoapVY+5avE9t4fequ5Og-ng7QN3bNsf1WQeeXT9HIrFddxd56E0gAY3IbIgfZTA9su8bB7hHn2yNRyJ6P4ZReDe+IvMSpLkpSr5bjWDQQnmSijnC5RaKexY6P49juL40YBEAA */
   id: "Dog",
   initial: "Asleep",
   states: {
@@ -9,6 +9,10 @@ export const dogMachine = createMachine({
       on: {
         "Samlls food": "Awake.Sleepy",
         "Loud noise": "Awake.Scared",
+      },
+
+      after: {
+        "10000": "Awake",
       },
     },
     Awake: {
@@ -19,8 +23,8 @@ export const dogMachine = createMachine({
       states: {
         Sleepy: {
           on: {
-            "Loud noise": "Scared"
-          }
+            "Loud noise": "Scared",
+          },
         },
 
         Scared: {},
@@ -28,13 +32,13 @@ export const dogMachine = createMachine({
           on: {
             "Give Treat": {
               target: "Begging",
-              internal: true
-            }
-          }
-        }
+              internal: true,
+            },
+          },
+        },
       },
 
-      initial: "Sleepy"
+      initial: "Sleepy",
     },
   },
 });
